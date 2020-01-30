3663c4a5f8ab02b682921c1690fdb8e014d68735
Jonas Geschke <github@yonny.de>
2020-01-30 12:39:25 +0100

    fixed list and added update functionality

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 #[macro_use]
 extern crate clap;
 
-use clap::{load_yaml, App, AppSettings::ColoredHelp, AppSettings::SubcommandRequired};
+use clap::{load_yaml, App, AppSettings::ColoredHelp};
 use spinners::{Spinners, Spinner};
 use std::path::PathBuf;
 use dirs::home_dir;
@@ -11,7 +11,6 @@ use std::fs::{create_dir, File};
 use std::io::{Write, Read};
 use colored::Colorize;
 use json::JsonValue;
-use futures::executor::block_on;
 use once_cell::sync::Lazy;
 
 static GITMOJI_URL : &'static str =
@@ -52,40 +51,48 @@ impl From<std::io::Error> for GitmojiError {
 fn main() {
     let yml = load_yaml!("main.yaml");
     let matches = App::from_yaml(yml)
-        .settings(&[ColoredHelp, SubcommandRequired])
+        .settings(&[ColoredHelp])
         .version(&crate_version!()[..])
         .author(&crate_authors!()[..])
         .set_term_width(80)
         .get_matches();
 
     if matches.is_present("list") {
-        if block_on(list_emojis()).is_err() {
+        if list_emojis(false).is_err() {
             eprintln!("Could not list gitmojis.");
+        }
+    }
+    if matches.is_present("update") {
+        if list_emojis(true).is_err() {
+            eprintln!("Could not update gitmojis.")
         }
     }
 
 }
 
-async fn list_emojis() -> Result<(), GitmojiError> {
-    fetch_emojis().await?;
+fn list_emojis(refetch: bool) -> Result<(), GitmojiError> {
+    if !GITMOJI_CACHE.exists() || refetch {
+        fetch_emojis()?;
+    }
     let emojis = get_emojis()?;
     print_emojis(emojis);
     Ok(())
 }
 
-async fn fetch_emojis() -> Result<(), GitmojiError> {
+fn fetch_emojis() -> Result<(), GitmojiError> {
     let sp = Spinner::new(Spinners::Dots9, "Fetching the emoji list".into());
-    let response: String = reqwest::get(GITMOJI_URL).await?.text().await?;
+    let response: String = reqwest::blocking::get(GITMOJI_URL)?.text()?;
     create_emoji_cache(json::parse(response.as_str())?)?;
     sp.stop();
+    print!("\r");
     Ok(())
 }
 
 fn create_emoji_cache(emojis: JsonValue) -> Result<(), GitmojiError> {
     if !GITMOJI_CACHE.exists() {
         create_dir(GITMOJI_CACHE.parent().expect("should have parent!"))?;
-        File::with_options().create(true).write(true).open(GITMOJI_CACHE.clone())?.write_all(emojis.dump().as_bytes())?;
     }
+    File::with_options().create(true).write(true).open(GITMOJI_CACHE.clone())?.write_all(emojis.dump().as_bytes())?;
     Ok(())
 }
 
@@ -96,8 +103,8 @@ fn get_emojis() -> Result<JsonValue, GitmojiError> {
 }
 
 fn print_emojis(emojis: JsonValue) {
-    if let JsonValue::Object(obj) = emojis {
-        for (_key, gitmoji) in obj.iter() {
+    if let JsonValue::Array(obj) = emojis["gitmojis"].clone() {
+        for gitmoji in obj.iter() {
             println!("{} - {} - {}", gitmoji["emoji"], gitmoji["code"].to_string().blue(), gitmoji["description"]);
         }
     }
